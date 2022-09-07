0a9f369f04b6e4fa358cd50f26a7f68e0c8c0a94
CallMeSam <112816535+REC-SAM@users.noreply.github.com>
2022-09-07 11:38:55 +0530

    Update Armstrong_Numbers.c

--- Armstrong_Numbers.c
+++ Armstrong_Numbers.c
@@ -29,7 +29,7 @@ int isArmstrongNumber( int num )
     long int sum = 0;
     int no_of_digits = findNumberOfDigit(num);
     for( int n = num; n > 0; n /= 10 )
-        sum += pow(num%10,no_of_digits);
+        sum += pow(n%10,no_of_digits);
     return ( sum == num ) ? 1 : 0; 
 }
 
@@ -37,13 +37,14 @@ void printAllArmstrongNumbers ( int max )
 {
     for( int i = 0; i <= max; ++i )
     {
-        if(isArmstrongNumber(i))
+        if( i < 10 || isArmstrongNumber(i) )
             printf("%d ",i);
     }
+    return ;
 }
 
 // Input / Output :-
 // ---------------
-//     Enter the Max range : 10
+//     Enter the Max range : 400
 //     The Armstrong Numbers are :
-//     0 1 2 3 4 5 6 7 8 9 
+//     0 1 2 3 4 5 6 7 8 9 153 370 371
